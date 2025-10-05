777975cf8adc5e0b901009d606a99c79bb5fd8ac
jgmccormick <john.g.mccormick@outlook.com>
2025-10-05 18:03:48 -0400

    Changed bookbot to use path following main.py when run, removed hardcoded path

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import sys
 from stats import (
     get_num_words, 
     chars_dict_to_sorted_list,
     get_chars_dict,
 )
+try:
+    open(sys.argv[1])
+except:
+    sys.exit("Usage: python3 main.py <path_to_book>")
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     # book_path = "README.md"
     text = get_book_text(book_path)
     num_words = get_num_words(text)

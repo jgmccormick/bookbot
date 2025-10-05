ba1c631b2fd9db17db4dac48495040b9d81fdb80
jgmccormick <john.g.mccormick@outlook.com>
2025-10-05 15:56:15 -0400

    updated main.py and stats.py to mirror solution files

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from stats import get_num_words
-from stats import get_num_chars
+from stats import get_num_words, get_chars_dict
 
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        return file_contents
+def main():
+    book_path = "books/frankenstein.txt"
+    # book_path = "README.md"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    character_count = get_chars_dict(text)
+    print(f"Found {num_words} total words")
+    print(character_count)
 
-def main(path_to_file):
-    file_contents = get_book_text(path_to_file)
-    num_words = get_num_words(file_contents)
-    character_count = get_num_chars(file_contents)
-    return print(f"Found {num_words} total words"), print(character_count)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-main("books/frankenstein.txt")
-# main("README.md")
+main()
--- stats.py
+++ stats.py
@@ -1,15 +1,14 @@
-def get_num_words(file_contents):
-    words = file_contents.split()
-    num_words = len(words)
-    return num_words
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def get_num_chars(file_contents):
-    lower_case = file_contents.lower()
-    character_count = {}
-    for character in lower_case:
-        if character in character_count:
-            character_count[character] += 1
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            character_count[character] = 1
-    return character_count
+            chars[lowered] = 1
+    return chars
 

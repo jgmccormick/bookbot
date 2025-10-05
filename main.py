1041396c6d0983f100a308033fa43a388c5214c7
jgmccormick <john.g.mccormick@outlook.com>
2025-10-05 17:11:17 -0400

    My solution to bookbot

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, chars_dict_sorted
 
 def main():
     book_path = "books/frankenstein.txt"
     # book_path = "README.md"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    character_count = get_chars_dict(text)
+    chars_dict = get_chars_dict(text)
+    sorted_chars = chars_dict_sorted(chars_dict)
+    print(("="*12) + " BOOKBOT " + ("="*12))
+    print("Analyzing book found at books/frankenstein.txt")
+    print(("-"*11) + " Word Count " + ("-"*11))
     print(f"Found {num_words} total words")
-    print(character_count)
+    print(("-"*9) + " Character Count " + ("-"*9))
+    for d in sorted_chars:
+        if d["char"].isalpha() is True:
+            print(f'{d["char"]}: {d["num"]}')
+    print(("="*13) + " END " + ("="*13))
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -12,3 +12,15 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
+def chars_dict_sorted(chars):
+    chars_sorted = []
+    for char, num in chars.items():
+        char_dict = {"char": char, "num": num}
+        chars_sorted.append(char_dict)
+
+    def sort_on(items):
+        return items["num"]
+
+    chars_sorted.sort(reverse=True, key=sort_on)
+    return chars_sorted
+

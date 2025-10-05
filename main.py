1bc8915eba7a27e4ed5a6935ba516d6bda70d64e
jgmccormick <john.g.mccormick@outlook.com>
2025-10-04 22:59:22 -0400

    main.py changed to count words

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def get_book_text(path_to_file):
         file_contents = f.read()
         return file_contents
 
+def word_count(file_contents):
+    words = file_contents.split()
+    num_words = len(words)
+    return num_words
 
 def main(path_to_file):
-    return print(get_book_text(path_to_file))
+    file_contents = get_book_text(path_to_file)
+    num_words = word_count(file_contents)
+    return print(f"Found {num_words} total words")
 
 main("books/frankenstein.txt")

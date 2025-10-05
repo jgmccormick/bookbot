74e5ccaebdfb02b0c1bacab383d3936b6a10ecf7
jgmccormick <john.g.mccormick@outlook.com>
2025-10-04 23:07:12 -0400

    created stats.py, changed word_count to get_num_words and moved to stats.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
-def word_count(file_contents):
-    words = file_contents.split()
-    num_words = len(words)
-    return num_words
-
 def main(path_to_file):
     file_contents = get_book_text(path_to_file)
-    num_words = word_count(file_contents)
+    num_words = get_num_words(file_contents)
     return print(f"Found {num_words} total words")
 
 main("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(file_contents):
+    words = file_contents.split()
+    num_words = len(words)
+    return num_words
